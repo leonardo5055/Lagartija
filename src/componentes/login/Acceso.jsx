a1e39a0d82be8d359884886a7c6631a2a4ec114f
unknown <lablanca.martinet36@gmail.com>
2024-08-28 00:40:36 +0200

    login terminado

--- src/componentes/login/Acceso.jsx
+++ src/componentes/login/Acceso.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Footer from '../estructura/Footer'
 import Nav from '../estructura/Nav'
 import Usuario from "../../img/usuario.png";
+import "./estilo/acceso.css"
 
 function Acceso() {
     return (
         <div>
             <Nav />
-            <div className='d-flex align-items-center justify-content-center gap-5'>
+            <div className='d-flex align-items-center justify-content-center gap-5 my-5'>
                 <div>
                     <img src={Usuario} width="150px" alt="usuario" />
                 </div>
@@ -22,8 +23,14 @@ function Acceso() {
                             <input type="password" className="form-control" id="contra" placeholder="Contraseña" />
                         </div>
                     </form>
-                    <div>
-                        <button className='px-4 py-2 rounded-pill w-100 text-light border-0'>Continuar</button>
+                    <div className='row row-cols-2'>
+                        <div className='col'>
+                            <button className='button1 mb-2 px-4 py-2 rounded-pill w-100 text-light border-0'>Continuar</button>
+                            <button className='button3 px-4 py-2 rounded-pill w-100 text-light border-0'>Olvide mi contraseña</button>
+                        </div>
+                        <div className='col'>
+                            <button className='button2 px-4 py-2 rounded-pill w-100 text-light border-0'>Crear cuenta</button>
+                        </div>
                     </div>
                 </div>
             </div>
